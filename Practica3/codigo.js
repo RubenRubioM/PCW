8bd14f15412fc47d3c51004a8106563d3adbc7ac
Ruben <rubensipala@gmail.com>
2018-05-11 18:27:30 +0200

    hasta copiar imagen con rallas y todo

--- Practica3/codigo.js
+++ Practica3/codigo.js
@@ -1,6 +1,15 @@
+////////////////////////////////////////////////////////////
+// Hacer la documentacion en el acerca.html              //
+//////////////////////////////////////////////////////////
+
 var _ANCHO_ = 360,
     _ALTO_  = 240;
 
+function redireccion(){
+	location.href = './index.html';
+}
+
+//Prepara el canvas y el drag&drop
 function prepararCanvas(){
 
 	let cvs = document.querySelectorAll('canvas');
@@ -10,11 +19,31 @@ function prepararCanvas(){
 		e.height = _ALTO_;
 	});
 
+	//Escribimos el texto en el canvas
+	let cv01 = document.querySelector('#cv01'),
+		ctx01= cv01.getContext('2d');
+
+	ctx01.textAlign = 'center';
+	ctx01.font = '40px sans-serif';
+	ctx01.strokeText('Click aqui',180,120);
 	//IMPLEMENTACION DE DRAG AND DROP
-	let cv01 = document.querySelector('#cv01');
+	
 	cv01.ondragover = function(e){
 		e.stopPropagation();
 		e.preventDefault(); //return false
+	}
+
+	cv01.ondragenter = function(e){
+		e.stopPropagation();
+		e.preventDefault();
+		cv01.style.backgroundImage = 'url(./Images/drag-drop-upload-1.gif)';
+	}
+
+	cv01.ondragleave = function(e){
+		e.stopPropagation();
+		e.preventDefault();
+		console.log('Sale');
+		cv01.style.backgroundImage = 'none';
 	}
 
 	cv01.ondrop = function(e){
@@ -36,6 +65,11 @@ function prepararCanvas(){
 					let ctx = cv01.getContext('2d');
 
 					ctx.drawImage(img,0,0,cv01.width,cv01.height);
+					copiarImagen();
+					dibujarLineas();
+					
+
+
 				};
 				img.src = fr.result;
 			};
@@ -44,6 +78,13 @@ function prepararCanvas(){
 		}
 
 	}
+}
+
+//Para que al darle click en el canvas se abra el input[file]
+function clickCanvas01(){
+	let input = document.getElementById('input-archivo');
+
+	input.click();
 }
 
 //Se llama cada vez que se selecciones una imagen en el input
@@ -58,7 +99,8 @@ function seleccionadaImagen(e){
 			let ctx = cv01.getContext('2d');
 
 			ctx.drawImage(img,0,0,cv01.width,cv01.height);
-			empezarJuego();
+			dibujarLineas();
+
 		};
 		img.src = fr.result;
 	};
@@ -67,39 +109,129 @@ function seleccionadaImagen(e){
 
 }
 
+//Dibuja lineas
 function dibujarLineas(){
+	let cv    = document.querySelector('#cv02'),
+		ctx   = cv.getContext('2d'),
+		nivel = document.getElementById('dificultad').value,
+		columnas,
+		filas,
+		dim;
 
-	let cv = document.querySelector('#cv02'),
-		ctx = cv.getContext('2d'),
-		r   = 3;
-		dim = cv.width / r;
+	//Siemrpe limpiaremos el canvas antes de dibujar las lineas
+	cv.width = cv.width;
+	copiarImagen();
+
+	if(nivel=='Facil'){
+		columnas = 6;
+		filas    = 4;
+		dim      = 60;
+	}else if(nivel=='Medio'){
+		columnas = 9;
+		filas    = 6;
+		dim      = 40;
+	}else if(nivel=='Dificil'){
+		columnas = 12;
+		filas    = 8;
+		dim      = 30;
+	}
+
+	console.log('Se van a construir '+columnas+' columnas y '+filas+' filas...');
+	ctx.beginPath();
+	ctx.lineWidth = 2;
+	ctx.strokeStyle = document.getElementById('colorPicker').value;
+
+	//Dibujamos las columnas
+	for(let i = 0; i<columnas; i++){
+		//lineas verticales
+		ctx.moveTo(i * dim,0);
+		ctx.lineTo(i * dim,cv.height);
+	} 
+
+	ctx.stroke();	
 
 	ctx.beginPath();
 	ctx.lineWidth = 2;
 	ctx.strokeStyle = document.getElementById('colorPicker').value;
-	for(let i = 0; i<r; i++){
+
+	//Dibujamos las filas
+	for(let i = 0; i<filas; i++){
 		//lineas verticales
-		ctx.moveTo(i * dim,0);
-		ctx.lineTo(i * dim,cv.height);
-
-
-		ctx.moveTo(0,i*dim);
-		ctx.lineTo(cv.width,i*dim);
-
+		ctx.moveTo(0,i * dim);
+		ctx.lineTo(cv.width,i * dim);
 	} 
 
 	ctx.stroke();	
 
 }
 
-function empezarJuego(){
+//Copia la imagen del canvas1 al canvas2
+function copiarImagen(){
 	let cv01  = document.querySelector('#cv01'),
 		ctx01 = cv01.getContext('2d'),	
-		cv02 = document.querySelector('#cv02'),
-		ctx02 = cv02.getContext('2d');
+		cv02  = document.querySelector('#cv02'),
+		ctx02 = cv02.getContext('2d'),
+		jugar = document.getElementById('jugar');
+
+	jugar.disabled = false;
 
 	let imgData = ctx01.getImageData(0,0,cv01.width,cv01.height);
 
 	//El putImageData tiene muchas signaturas utiles para la práctica
 	ctx02.putImageData(imgData,0,0);
+
+}
+
+//Se llama al darle click en jugar e inicia el cronometro
+function iniciarJuego(){
+	control = setInterval(cronometro,10);
+}
+
+var centesimas = 0;
+var segundos = 0;
+var minutos = 0;
+var horas = 0;
+
+function reinicio () {
+	clearInterval(control);
+	centesimas = 0;
+	segundos = 0;
+	minutos = 0;
+	horas = 0;
+	Centesimas.innerHTML = ":00";
+	Segundos.innerHTML = ":00";
+	Minutos.innerHTML = ":00";
+	Horas.innerHTML = "00";
+}
+
+function cronometro () {
+	if (centesimas < 99) {
+		centesimas++;
+		if (centesimas < 10) { centesimas = "0"+centesimas }
+		Centesimas.innerHTML = ":"+centesimas;
+	}
+	if (centesimas == 99) {
+		centesimas = -1;
+	}
+	if (centesimas == 0) {
+		segundos ++;
+		if (segundos < 10) { segundos = "0"+segundos }
+		Segundos.innerHTML = ":"+segundos;
+	}
+	if (segundos == 59) {
+		segundos = -1;
+	}
+	if ( (centesimas == 0)&&(segundos == 0) ) {
+		minutos++;
+		if (minutos < 10) { minutos = "0"+minutos }
+		Minutos.innerHTML = ":"+minutos;
+	}
+	if (minutos == 59) {
+		minutos = -1;
+	}
+	if ( (centesimas == 0)&&(segundos == 0)&&(minutos == 0) ) {
+		horas ++;
+		if (horas < 10) { horas = "0"+horas }
+		Horas.innerHTML = horas;
+	}
 }
