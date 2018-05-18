08185758e3f65bd99bcc20b71f90bf3fd9f9ccb2
Ruben <rubensipala@gmail.com>
2018-05-18 12:14:25 +0200

    SE DESORDENAN

--- Practica3/codigo.js
+++ Practica3/codigo.js
@@ -6,7 +6,12 @@ var _ANCHO_ = 360,
     _ALTO_  = 240,
     filas,
     columnas,
-    dim;
+    dim,
+    img,
+    juego_empezado = false,
+    piezas=[],
+	arrayOrdenado=[],
+	arrayDesordenado=[];
 
 function redireccion(){
 	location.href = './index.html';
@@ -64,7 +69,7 @@ function prepararCanvas(){
 
 			console.log('Es una imagen valida...');
 			fr.onload = function(){
-				let img = new Image();
+				 img = new Image();
 
 				img.onload = function(){
 					let ctx = cv01.getContext('2d');
@@ -84,8 +89,6 @@ function prepararCanvas(){
 
 	}
 
-
-
 }
 
 //Para que al darle click en el canvas se abra el input[file]
@@ -101,7 +104,7 @@ function seleccionadaImagen(e){
 	let fr = new FileReader();
 
 	fr.onload = function(){
-		let img = new Image();
+		 img = new Image();
 
 		img.onload = function(){
 			let ctx = cv01.getContext('2d');
@@ -124,8 +127,15 @@ function dibujarLineas(){
 		nivel = document.getElementById('dificultad').value;
 
 	//Siemrpe limpiaremos el canvas antes de dibujar las lineas
-	cv.width = cv.width;
-	copiarImagen();
+
+	//Comprobamos si el juego esta iniciado porque si no se realizara otro metodo de borrado
+	if(!juego_empezado){
+		cv.width = cv.width;
+		copiarImagen();
+	}else{
+		//El juego esta empezado
+	}
+	
 
 	if(nivel=='Facil'){
 		columnas = 6;
@@ -141,6 +151,7 @@ function dibujarLineas(){
 		dim      = 30;
 	}
 
+	console.log('Dificultad: '+nivel+'...');
 	console.log('Se van a construir '+columnas+' columnas y '+filas+' filas...');
 	ctx.beginPath();
 	ctx.lineWidth = 2;
@@ -194,20 +205,144 @@ function iniciarJuego(){
 	document.getElementById('ayuda').disabled = false;
 	document.getElementById('Segundos').style.display = 'block';
 
+	juego_empezado=true;
 	desordenarPiezas();
 	control = setInterval(cronometro,1000);
 }
 
-
+/*
 function desordenarPiezas(){
+	var i,
+		pieza,
+		xpos = 0,
+		ypos = 0;
 
+	let cv02 = document.getElementById('cv02');
+	for(i=0;i<filas*columnas;i++){
+		pieza = {};
+		pieza.sx = xpos;
+		pieza.sy = ypos;
+
+		piezas.push(pieza);
+		xpos+=dim;
+
+		if(xpos>=cv02.width){
+			xpos=0;
+			ypos+=dim;
+		}
+	}
+
+	mezclarArray();
+}
+
+function mezclarArray(){
+	
+
+	piezas = desordenarArray(piezas);
+	
+	let cv02 = document.getElementById('cv02');
+	let ctx02= cv02.getContext('2d');
+	ctx02.clearRect(0,0,cv02.width,cv02.height);
+	var i;
+	var pieza;
+	var xpos=0;
+	var ypos=0;
+
+	for(i=0;i<piezas.length;i++){
+		pieza = piezas[i];
+		pieza.xpos = xpos;
+		pieza.ypos = ypos;
+		ctx02.drawImage(img, pieza.sx, pieza.sy, dim, dim, xpos, ypos, dim, dim);
+		ctx02.strokeRect(xpos,ypos,dim,dim);
+		xpos+=dim;
+
+		if(xpos>=cv02.width){
+			xpos=0;
+			ypos+=dim;
+		}
+	}
+
+	//dibujarLineas();
 }
 
 
-var centesimas = 0;
+function desordenarArray(a) {
+	console.log(a);
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+
+    console.log(a);
+    return a;
+}
+*/
+
+function desordenarPiezas(){
+	for(let i=0;i<filas*columnas;i++){
+		arrayOrdenado[i]=i;
+
+	}
+	arrayDesordenado=desordenarArray();
+
+	pintarCanvasDesordenado();
+}
+
+
+function desordenarArray(){
+	var a = [];
+	for(let i=0;i<arrayOrdenado.length;i++){
+		a[i]=arrayOrdenado[i];
+	}
+	
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+
+    
+    return a;
+}
+
+function pintarCanvasDesordenado(){
+	var cv01  = document.querySelector('#cv01'),
+		ctx01 = cv01.getContext('2d'),	
+		cv02  = document.querySelector('#cv02'),
+		ctx02 = cv02.getContext('2d');
+
+	var i,
+		pieza,
+		xpos = 0,
+		ypos = 0;
+
+	//Al array piezas le voy asignando cada posicion un objeto con la posicion X e Y de esos sitios, por ejemplo en la posicion (1,1) = (60,60)
+	for(i=0;i<filas*columnas;i++){
+		pieza = {};
+		pieza.sx = xpos;
+		pieza.sy = ypos;
+
+		piezas.push(pieza);
+		xpos+=dim;
+		if(xpos>=cv02.width){
+			xpos=0;
+			ypos+=dim;
+		}
+	}
+
+	cv02.width =cv02.width;
+	for(let i=0;i<arrayDesordenado.length;i++){
+		let imgdata    = ctx01.getImageData(piezas[arrayDesordenado[i]].sx,piezas[arrayDesordenado[i]].sy,dim,dim);
+
+
+		ctx02.putImageData(imgdata,piezas[arrayOrdenado[i]].sx,piezas[arrayOrdenado[i]].sy);
+
+		console.log('Imagen colocada en '+arrayDesordenado[i]+' ('+piezas[arrayDesordenado[i]].sx+','+piezas[arrayDesordenado[i]].sy+')');
+
+	}
+
+}
+
 var segundos = 0;
-var minutos = 0;
-var horas = 0;
 
 function reinicio () {
 	clearInterval(control);
@@ -217,5 +352,4 @@ function reinicio () {
 function cronometro () {
 	segundos++;
 	Segundos.innerHTML = segundos+"s";
-	console.log(segundos);
 }
